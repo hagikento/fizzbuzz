d0ab2aa7b3653bb66e7386903fe4f8b71869778f
hagikento <doradora.0141@gmail.com>
2020-09-20 16:28:18 +0900

    update main file

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    for i in range(1, 31):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        else:
+            print(i)
+
+
+if __name__ == "__main__":
+    main()
